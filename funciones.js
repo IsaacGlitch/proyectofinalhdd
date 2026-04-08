4cd666139c4d4f7170d35dc020027c74e35c99da
CarlosInspire20 <carlosernestog633@gmail.com>
2026-04-08 16:48:13 -0500

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,3 +1,22 @@
 function agregarCarrito(producto) {
     alert(producto + " agregado al carrito 🛒");
 }
+
+function abrirLogin() {
+    document.getElementById("modalLogin").style.display = "block";
+}
+
+function cerrarLogin() {
+    document.getElementById("modalLogin").style.display = "none";
+}
+
+function iniciarSesion() {
+    let user = document.getElementById("usuario").value;
+
+    if (user === "") {
+        alert("Por favor ingresa un usuario");
+    } else {
+        alert("Bienvenido " + user + " 🎮");
+        cerrarLogin();
+    }
+}
